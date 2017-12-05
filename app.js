569bb24b985b54e56381fa612cfb5423e844ee1b
Lễ Bùi <buingocle.vp@gmail.com>
2017-12-05 22:01:00 +0700

    add mongo db

--- app.js
+++ app.js
@@ -1,21 +1,55 @@
 var express = require('express');
-var app = express();
+const mongodb = require('mongodb');
+const nconf = require('nconf');
+const storage = require('./storage')
+nconf.argv().env().file('keys.json');
 
-// Viewing engine
-app.set('view engine', 'ejs');
+const user = nconf.get('mongoUser');
+const pass = nconf.get('mongoPass');
+const host = nconf.get('mongoHost');
+const port = nconf.get('mongoPort');
 
-var routes = require('./routes');
-var path = require('path');
-app.use(express.static(path.join(__dirname, 'public')));
+const createServer = () => {
+	var app = express();
 
-// Routes
-// Home Route
-app.get('/', routes.home);
+	// Viewing engine
+	app.set('view engine', 'ejs');
 
-app.get('/star_wars_episode/:episode_number?', routes.movie_single);
+	var routes = require('./routes');
+	var path = require('path');
+	app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('*', routes.notfound);
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, function () {
-	console.log('App server running on localhost: ', PORT)
-});
+	// Routes
+	// Home Route
+	app.get('/', routes.home);
+
+	app.get('/star_wars_episode/:episode_number?', routes.movie_single);
+
+	app.get('*', routes.notfound);
+	const PORT = process.env.PORT || 8080;
+	app.listen(PORT, function () {
+		console.log('App server running on localhost: ', PORT)
+	});
+}
+
+// [START client]
+let uri = `mongodb://${user}:${pass}@${host}:${port}`;
+if (nconf.get('mongoDatabase')) {
+	uri = `${uri}/${nconf.get('mongoDatabase')}`;
+}
+console.log('connecting to:', uri);
+try {
+	mongodb.MongoClient.connect(uri, (err, db) => {
+		if (err) {
+			console.log('can not connect to database with error: ', err);
+		} else {
+			console.log('connected to database');
+		}
+
+		storage.mongo = db;
+		createServer();
+	});
+} catch (error) {
+	console.log('error: ', error);
+	createServer();
+}
